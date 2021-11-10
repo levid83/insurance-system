066bcc8f49a8630bc014d5a383dc5425108866cf
Levente Danel <levente.danel@gmail.com>
2021-11-10 22:48:35 +0200

    create the contract service class

---
+++ contracts/src/services/ContractService.ts
@@ -0,0 +1,18 @@
+import { EntityManager } from "typeorm";
+import { Contract } from "../entities/Contract";
+
+export default class ContractService {
+  static async createContract(db: EntityManager, data: any) {
+    const contract = new Contract();
+    contract.id = data.contractId;
+    contract.start_date = new Date(data.startDate);
+    contract.premium = data.premium;
+    await db.save(contract);
+  }
+
+  static async terminateContract(db: EntityManager, data: any) {
+    const contract = await db.findOne(Contract, data.contractId);
+    contract.termination_date = new Date(data.terminationDate);
+    await db.save(contract);
+  }
+}
