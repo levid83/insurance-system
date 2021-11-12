99a091433856cb434cd1187d234121678ba8648b
Levente Danel <levente.danel@gmail.com>
2021-11-12 19:12:28 +0200

    create custom react hook for api requests

---
+++ frontend/src/hooks/useService.js
@@ -0,0 +1,27 @@
+import { useState, useCallback } from "react";
+
+const UseService = (serviceFunction, initialState) => {
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(initialState);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const callService = useCallback(
+    (props = {}) => {
+      setIsLoading(true);
+      return serviceFunction(props)
+        .then((data) => {
+          setData(data);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          setError(err);
+          setIsLoading(false);
+        });
+    },
+    [serviceFunction]
+  );
+
+  return [data, callService, error, isLoading];
+};
+
+export default UseService;
